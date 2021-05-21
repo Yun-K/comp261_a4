8ffb7b128d6e8f26fd0b4b19962b3e817bab3781
Yun Zhou <zhouyun@ecs.vuw.ac.nz>
2021-05-21 17:05:29 +0800

    add the skeleton SEN and its subclasses

--- 261_a4_yun/src/COND.java
+++ 261_a4_yun/src/COND.java
@@ -5,6 +5,62 @@
  * @author Yun Zhou 300442776
  * @version
  */
-public class COND {
+public interface COND extends RobotProgramNode {
+    /**
+     * Description: <br/>
+     * Check if the CONDITION is met, if the condition is true, return true. otherwise return
+     * false.
+     * <p>
+     * e.g. lt(fuelLeft, 20) will check if the fuelLeft of the specificied robot is less than
+     * 20.
+     * 
+     * @author Yun Zhou
+     * @param robot
+     * @return true if the Sensor is less than numNode
+     */
+    public abstract boolean compute(Robot robot);
 
+    /**
+     * Should not execute the COND.execute() as well as the subClasses.
+     * 
+     * @see RobotProgramNode#execute(Robot)
+     */
+    @Override
+    public default void execute(Robot robot) {
+        assert false;
+    }
+
+}
+
+class lt implements COND {
+
+    private EXPR sensorNode, numExprNode;
+
+    /**
+     * A constructor. It construct a new instance of lt.
+     *
+     * @param sensorNode
+     * @param expressionNode
+     */
+    public lt(EXPR sensorNode, EXPR expressionNode) {
+        super();
+        this.sensorNode = sensorNode;
+        this.numExprNode = expressionNode;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer("lt(")
+                .append(sensorNode.toString())
+                .append(",")
+                .append(numExprNode.toString())
+                .append(")");
+        return sb.toString();
+
+    }
+
+    @Override
+    public boolean compute(Robot robot) {
+        return this.sensorNode.compute(robot) < this.numExprNode.compute(robot) ? true : false;
+    }
 }
